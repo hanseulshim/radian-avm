eb85cc5ea98c36ba7ca9f62093a6a853d0604de2
Hanseul Shim <hanseulshim@gmail.com>
2020-05-21 09:44:38 -0400

    Add testing scripts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,6 @@ import { DataInterface, initialState } from 'initialState'
 import React, { createContext, useEffect, useState } from 'react'
 import Cover from './Cover'
 import Demographics from './Demographics'
-import MarketPerformance1 from './MarketPerformance1'
 import MarketPerformance2 from './MarketPerformance2'
 import SubjectPropertyDescriptionDetails from './SubjectPropertyDescriptionDetails'
 
@@ -13,7 +12,7 @@ const App = () => {
 	useEffect(() => {
 		const getData = async () => {
 			const response = await fetch(
-				process.env.REACT_APP_STAGE === 'local'
+				process.env.REACT_APP_ENV === 'local'
 					? `http://localhost:4000/dev`
 					: `https://4b7f4j8t8l.execute-api.us-east-1.amazonaws.com/dev/`
 			)
@@ -24,7 +23,7 @@ const App = () => {
 		getData()
 	}, [])
 
-	const test = true
+	const test = process.env.REACT_APP_STAGE === 'test'
 
 	return (
 		<DataContext.Provider value={data}>
@@ -35,7 +34,7 @@ const App = () => {
 					<>
 						<Cover />
 						<SubjectPropertyDescriptionDetails />
-						<MarketPerformance1 />
+						{/* <MarketPerformance1 /> */}
 						<MarketPerformance2 />
 						<Demographics />
 					</>
