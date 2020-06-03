f961be88f959f9db18cfbfb2ca3789625ba4bf8a
joneil@boostlabs.com <joneil@boostlabs.com>
2020-06-02 20:48:23 -0500

    add bullets to property value trend

--- src/components/SubjectPropertyHistory/PropertyValueTrendChart.tsx
+++ src/components/SubjectPropertyHistory/PropertyValueTrendChart.tsx
@@ -4,10 +4,33 @@ import * as am4core from '@amcharts/amcharts4/core'
 import * as am4charts from '@amcharts/amcharts4/charts'
 import { colors } from 'styles/colors'
 
+interface event {
+	type: string | null
+	daysOnMarket: number | null
+	listedDate: string | null
+	listedAmount: number | null
+	listedSqFtPrice: number | null
+	action: string | null
+	actionDate: string | null
+	actionAmount: number | null
+	actionSqFtPrice: number | null
+	imagePath: string | null
+}
+
 const PropertyValueTrendChart = () => {
-	const { propertyValueTrend } = useContext(DataContext)
+	const { propertyValueTrend, subjectPropertyTransactions } = useContext(
+		DataContext
+	)
 	const { averagePrice, medianPriceLevel1, medianPriceLevel2, hpi } = {
 		...propertyValueTrend
+	}
+
+	let event1: event, event2: event, event3: event
+
+	if (subjectPropertyTransactions) {
+		event1 = subjectPropertyTransactions[0]
+		event2 = subjectPropertyTransactions[1]
+		event3 = subjectPropertyTransactions[2]
 	}
 
 	useEffect(() => {
@@ -15,6 +38,7 @@ const PropertyValueTrendChart = () => {
 		chart.paddingTop = 10
 		chart.paddingLeft = 0
 		chart.paddingBottom = 0
+		chart.paddingRight = 50
 
 		const dateAxis = chart.xAxes.push(new am4charts.DateAxis())
 		dateAxis.baseInterval = {
@@ -32,7 +56,6 @@ const PropertyValueTrendChart = () => {
 		valueAxis.renderer.minGridDistance = 50
 		valueAxis.renderer.labels.template.dx = -10
 		valueAxis.numberFormatter.numberFormat = '$#a'
-		valueAxis.extraMax = 0.2
 
 		const level1Series = chart.series.push(new am4charts.LineSeries())
 		level1Series.data = medianPriceLevel1?.data as any
@@ -69,7 +92,65 @@ const PropertyValueTrendChart = () => {
 		retailVsDistressedSeries.fillOpacity = 0.1
 		retailVsDistressedSeries.stroke = am4core.color(colors.gray)
 		retailVsDistressedSeries.strokeOpacity = 0.1
-	}, [propertyValueTrend])
+
+		const event1Series = chart.series.push(new am4charts.ColumnSeries())
+		event1Series.data = [{ ...event1 }]
+		event1Series.dataFields.dateX = 'actionDate'
+		event1Series.dataFields.valueY = 'actionAmount'
+		event1Series.columns.template.width = 0
+		event1Series.columns.template.strokeOpacity = 0
+		const bullet = event1Series.bullets.push(new am4core.Circle())
+		bullet.stroke = am4core.color(colors.black)
+		bullet.fill = am4core.color(colors.black)
+		bullet.strokeOpacity = 1
+		bullet.fillOpacity = 1
+		bullet.strokeWidth = 3
+		bullet.width = 25
+		bullet.height = 25
+		const labelBullet = event1Series.bullets.push(new am4charts.LabelBullet())
+		labelBullet.label.text = '1'
+		labelBullet.label.stroke = am4core.color(colors.white)
+		labelBullet.label.strokeOpacity = 10
+		labelBullet.label.zIndex = 5
+
+		const event2Series = chart.series.push(new am4charts.ColumnSeries())
+		event2Series.data = [{ ...event2 }]
+		event2Series.dataFields.dateX = 'actionDate'
+		event2Series.dataFields.valueY = 'actionAmount'
+		event2Series.columns.template.width = 0
+		const bullet2 = event2Series.bullets.push(new am4core.Circle())
+		bullet2.stroke = am4core.color(colors.neptune)
+		bullet2.fill = am4core.color(colors.neptune)
+		bullet2.strokeOpacity = 1
+		bullet2.fillOpacity = 1
+		bullet2.strokeWidth = 3
+		bullet2.width = 25
+		bullet2.height = 25
+		const labelBullet2 = event2Series.bullets.push(new am4charts.LabelBullet())
+		labelBullet2.label.text = '2'
+		labelBullet2.label.stroke = am4core.color(colors.white)
+		labelBullet2.label.strokeOpacity = 10
+		labelBullet2.label.zIndex = 10
+
+		const event3Series = chart.series.push(new am4charts.ColumnSeries())
+		event3Series.data = [{ ...event3 }]
+		event3Series.dataFields.dateX = 'actionDate'
+		event3Series.dataFields.valueY = 'actionAmount'
+		event3Series.columns.template.width = 0
+		const bullet3 = event3Series.bullets.push(new am4core.Circle())
+		bullet3.stroke = am4core.color(colors.black)
+		bullet3.fill = am4core.color(colors.white)
+		bullet3.strokeOpacity = 1
+		bullet3.fillOpacity = 1
+		bullet3.strokeWidth = 3
+		bullet3.width = 25
+		bullet3.height = 25
+		const labelBullet3 = event3Series.bullets.push(new am4charts.LabelBullet())
+		labelBullet3.label.text = '3'
+		labelBullet3.label.stroke = am4core.color(colors.black)
+		labelBullet3.label.strokeOpacity = 10
+		labelBullet3.label.zIndex = 10
+	}, [propertyValueTrend, subjectPropertyTransactions])
 
 	return <div className="chart" id="propertyValueTrendChart"></div>
 }
